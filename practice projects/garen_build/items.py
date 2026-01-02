9578d2011a7fb042c51e74b453a355206fb5028b
Tianwei <90248677+CalvoSeko@users.noreply.github.com>
2026-01-02 12:19:21 +0100

    setup items class

--- practice projects/garen_build/items.py
+++ practice projects/garen_build/items.py
@@ -3,11 +3,13 @@ class shop_item:
         self, 
         name: str, 
         price: int, 
-        stats: dict[str, int]
+        stats: dict[str, int],
+        percent_dmg_amp: float = 0
     ):
         self._name = name
         self._price = price
         self._stats = stats
+        self._percent_dmg_amp = percent_dmg_amp    
     
     @property
     def name(self):
@@ -20,3 +22,23 @@ class shop_item:
     @property
     def stats(self):
         return self._stats
+
+    @property
+    def percent_dmg_amp(self):
+        return self._percent_dmg_amp
+
+class active_item(shop_item):
+    def __init__(
+        self, 
+        name: str, 
+        price: int, 
+        stats: dict[str, int],
+        percent_dmg_amp: float = 0,
+        active_dmg: float = 0
+    ):
+        super().__init__(name, price, stats, percent_dmg_amp)
+        self._active_dmg = active_dmg
+    
+    @property
+    def active_dmg(self):
+        return self._active_dmg
